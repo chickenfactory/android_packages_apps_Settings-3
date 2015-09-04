LineageOS/android_packages_apps_Settings

Settings: Long Term Orbits (LTO)
Patchset 2: Opt-In LtoService (if device supports LTO) and simplify the code
            Rebased
Patchset 3: Added LocationSettings suggestions
            Rebased

Require http://review.cyanogenmod.org/#/c/51421

Change-Id: I878104eb3c859168faa10467fab3772a2f639288
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

src/com/android/settings/LocationSettings.java
@@ -16,22 +16,31 @@
 
 package com.android.settings;
 
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.ContentQueryMap;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.os.UserManager;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
+
+import com.android.settings.cyanogenmod.LtoService;
+
+import org.cyanogenmod.hardware.LongTermOrbits;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -43,14 +52,16 @@ public class LocationSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     // Location Settings
-    private static final String KEY_LOCATION_TOGGLE = "location_toggle";
+    public static final String KEY_LOCATION_TOGGLE = "location_toggle";
     private static final String KEY_LOCATION_NETWORK = "location_network";
     private static final String KEY_LOCATION_GPS = "location_gps";
     private static final String KEY_ASSISTED_GPS = "assisted_gps";
+    public static final String KEY_GPS_DOWNLOAD_DATA_WIFI_ONLY = "gps_download_data_wifi_only";
 
     private CheckBoxPreference mNetwork;
     private CheckBoxPreference mGps;
     private CheckBoxPreference mAssistedGps;
+    private CheckBoxPreference mGpsDownloadDataWifiOnly;
     private SwitchPreference mLocationAccess;
 
     // These provide support for receiving notification when Location Manager settings change.
@@ -92,6 +103,8 @@ public class LocationSettings extends SettingsPreferenceFragment
         mNetwork = (CheckBoxPreference) root.findPreference(KEY_LOCATION_NETWORK);
         mGps = (CheckBoxPreference) root.findPreference(KEY_LOCATION_GPS);
         mAssistedGps = (CheckBoxPreference) root.findPreference(KEY_ASSISTED_GPS);
+        mGpsDownloadDataWifiOnly =
+                (CheckBoxPreference) root.findPreference(KEY_GPS_DOWNLOAD_DATA_WIFI_ONLY);
 
         // Only enable these controls if this user is allowed to change location
         // sharing settings.
@@ -101,6 +114,17 @@ public class LocationSettings extends SettingsPreferenceFragment
         if (mNetwork != null) mNetwork.setEnabled(isToggleAllowed);
         if (mGps != null) mGps.setEnabled(isToggleAllowed);
         if (mAssistedGps != null) mAssistedGps.setEnabled(isToggleAllowed);
+        if (mGpsDownloadDataWifiOnly != null) mGpsDownloadDataWifiOnly.setEnabled(isToggleAllowed);
+
+        if (!LongTermOrbits.isSupported()) {
+            root.removePreference(mGpsDownloadDataWifiOnly);
+            mGpsDownloadDataWifiOnly = null;
+        } else {
+            if (saveDownloadDataWifiOnlyPref(getActivity())) {
+                root.removePreference(mGpsDownloadDataWifiOnly);
+                mGpsDownloadDataWifiOnly = null;
+            }
+        }
 
         mLocationAccess.setOnPreferenceChangeListener(this);
         return root;
@@ -173,6 +197,9 @@ public class LocationSettings extends SettingsPreferenceFragment
                     Settings.Global.ASSISTED_GPS_ENABLED, 2) == 1);
             mAssistedGps.setEnabled(gpsEnabled);
         }
+        if (mGpsDownloadDataWifiOnly != null) {
+            mGpsDownloadDataWifiOnly.setEnabled(gpsEnabled);
+        }
     }
 
     /**
@@ -196,6 +223,7 @@ public class LocationSettings extends SettingsPreferenceFragment
         Settings.Secure.setLocationProviderEnabled(cr,
                 LocationManager.NETWORK_PROVIDER, checked);
         updateLocationToggles();
+        updateLtoServiceStatus(getActivity(), checked);
     }
 
     @Override
@@ -209,6 +237,50 @@ public class LocationSettings extends SettingsPreferenceFragment
     @Override
     public int getHelpResource() {
         return R.string.help_url_location_access;
+    }
+
+    private static void updateLtoServiceStatus(Context context, boolean start) {
+        Intent intent = new Intent(context, LtoService.class);
+        if (start) {
+            context.startService(intent);
+        } else {
+            context.stopService(intent);
+        }
+    }
+
+    /**
+     * Restore the properties associated with this preference on boot
+     * @param ctx A valid context
+     */
+    public static void restore(final Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        // Start the Lto Service
+        if (LongTermOrbits.isSupported() && prefs.getBoolean(KEY_LOCATION_TOGGLE, false)) {
+            saveDownloadDataWifiOnlyPref(context);
+
+            // Starts the LtoService, but delayed 2 minutes after boot (this should give a
+            // proper time to start all device services)
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(context, LtoService.class);
+            PendingIntent pi = PendingIntent.getService(context, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+            long nextLtoDownload = System.currentTimeMillis() + (1000 * 60 * 2L);
+            am.set(AlarmManager.RTC, nextLtoDownload, pi);
+        }
+    }
+
+    private static boolean saveDownloadDataWifiOnlyPref(Context context) {
+        PackageManager pm = context.getPackageManager();
+        boolean supportsTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+        boolean supportsWifi = pm.hasSystemFeature(PackageManager.FEATURE_WIFI);
+        if (!supportsWifi || !supportsTelephony) {
+            SharedPreferences.Editor editor =
+                    PreferenceManager.getDefaultSharedPreferences(context).edit();
+            editor.putBoolean(KEY_GPS_DOWNLOAD_DATA_WIFI_ONLY, supportsWifi);
+            editor.apply();
+            return true;
+        }
+        return false;
     }
 }
 

chickenfactory/android_packages_apps_Settings-3

Change-Id: I1fbdba644fc05d55fc096b47bfac3f9903383370
Conflicts:
	AndroidManifest.xml
	res/values/cm_strings.xml
	src/com/android/settings/cyanogenmod/BootReceiver.java

src/com/android/settings/location/LocationSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,26 +17,34 @@
 
 package com.android.settings.location;
 
+import static android.hardware.CmHardwareManager.FEATURE_LONG_TERM_ORBITS;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.CmHardwareManager;
 import android.location.SettingInjectorService;
-import android.os.Binder;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
-import android.provider.Settings;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.util.Log;
 import android.widget.Switch;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
+import com.android.settings.cyanogenmod.LtoService;
 import com.android.settings.widget.SwitchBar;
 
 import java.util.Collections;
@@ -68,7 +77,7 @@ import java.util.List;
  * implementation.
  */
 public class LocationSettings extends LocationSettingsBase
-        implements SwitchBar.OnSwitchChangeListener {
+        implements SwitchBar.OnSwitchChangeListener, OnPreferenceChangeListener {
 
     private static final String TAG = "LocationSettings";
 
@@ -90,12 +99,16 @@ public class LocationSettings extends LocationSettingsBase
     /** Key for preference category "Location services" */
     private static final String KEY_LOCATION_SERVICES = "location_services";
 
+    /** Key for preference LTO over Wi-Fi only */
+    public static final String KEY_LTO_DOWNLOAD_DATA_WIFI_ONLY = "lto_download_data_wifi_only";
+
     private SwitchBar mSwitchBar;
     private Switch mSwitch;
     private boolean mValidListener = false;
     private UserHandle mManagedProfile;
     private Preference mManagedProfilePreference;
     private Preference mLocationMode;
+    private SwitchPreference mLtoDownloadDataWifiOnly;
     private PreferenceCategory mCategoryRecentLocationRequests;
     /** Receives UPDATE_INTENT  */
     private BroadcastReceiver mReceiver;
@@ -182,6 +195,17 @@ public class LocationSettings extends LocationSettingsBase
                         return true;
                     }
                 });
+
+        mLtoDownloadDataWifiOnly =
+                (SwitchPreference) root.findPreference(KEY_LTO_DOWNLOAD_DATA_WIFI_ONLY);
+        if (mLtoDownloadDataWifiOnly != null) {
+            if (!isLtoSupported(activity) || !checkGpsDownloadWiFiOnly(activity)) {
+                root.removePreference(mLtoDownloadDataWifiOnly);
+                mLtoDownloadDataWifiOnly = null;
+            } else {
+                mLtoDownloadDataWifiOnly.setOnPreferenceChangeListener(this);
+            }
+        }
 
         mCategoryRecentLocationRequests =
                 (PreferenceCategory) root.findPreference(KEY_RECENT_LOCATION_REQUESTS);
@@ -304,6 +328,9 @@ public class LocationSettings extends LocationSettingsBase
         // Disable the whole switch bar instead of the switch itself. If we disabled the switch
         // only, it would be re-enabled again if the switch bar is not disabled.
         mSwitchBar.setEnabled(!restricted);
+        if (mLtoDownloadDataWifiOnly != null) {
+            mLtoDownloadDataWifiOnly.setEnabled(enabled && !restricted);
+        }
         mLocationMode.setEnabled(enabled && !restricted);
         mCategoryRecentLocationRequests.setEnabled(enabled);
 
@@ -346,5 +373,69 @@ public class LocationSettings extends LocationSettingsBase
         } else {
             setLocationMode(android.provider.Settings.Secure.LOCATION_MODE_OFF);
         }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (mLtoDownloadDataWifiOnly != null && preference.equals(mLtoDownloadDataWifiOnly)) {
+            updateLtoServiceStatus(getActivity(), isLocationModeEnabled(getActivity()));
+        }
+        return true;
+    }
+
+    private static void updateLtoServiceStatus(Context context, boolean start) {
+        Intent intent = new Intent(context, LtoService.class);
+        if (start) {
+            context.startService(intent);
+        } else {
+            context.stopService(intent);
+        }
+    }
+
+    private static boolean checkGpsDownloadWiFiOnly(Context context) {
+        PackageManager pm = context.getPackageManager();
+        boolean supportsTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+        boolean supportsWifi = pm.hasSystemFeature(PackageManager.FEATURE_WIFI);
+        if (!supportsWifi || !supportsTelephony) {
+            SharedPreferences.Editor editor =
+                    PreferenceManager.getDefaultSharedPreferences(context).edit();
+            editor.putBoolean(KEY_LTO_DOWNLOAD_DATA_WIFI_ONLY, supportsWifi);
+            editor.apply();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isLocationModeEnabled(Context context) {
+        int mode = android.provider.Settings.Secure.getInt(context.getContentResolver(),
+                android.provider.Settings.Secure.LOCATION_MODE,
+                android.provider.Settings.Secure.LOCATION_MODE_OFF);
+        return (mode != android.provider.Settings.Secure.LOCATION_MODE_OFF);
+    }
+
+    /**
+     * Restore the properties associated with this preference on boot
+     * @param ctx A valid context
+     */
+    public static void restore(final Context context) {
+        if (isLtoSupported(context) && isLocationModeEnabled(context)) {
+            // Check and adjust the value for Gps download data on wifi only
+            checkGpsDownloadWiFiOnly(context);
+
+            // Starts the LtoService, but delayed 2 minutes after boot (this should give a
+            // proper time to start all device services)
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(context, LtoService.class);
+            PendingIntent pi = PendingIntent.getService(context, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+            long nextLtoDownload = System.currentTimeMillis() + (1000 * 60 * 2L);
+            am.set(AlarmManager.RTC, nextLtoDownload, pi);
+        }
+    }
+
+    private static boolean isLtoSupported(Context context) {
+        final CmHardwareManager hwManager =
+                (CmHardwareManager) context.getSystemService(Context.CMHW_SERVICE);
+        return hwManager != null && hwManager.isSupported(FEATURE_LONG_TERM_ORBITS);
     }
 }